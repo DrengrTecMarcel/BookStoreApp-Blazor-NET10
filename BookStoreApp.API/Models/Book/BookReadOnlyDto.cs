b39473b8588a6ed4c099bf70e77de24441ce7b50
DrengrTecMarcel <Marcel.Langwald@Drengrtec-solutions.de>
2026-03-20 05:22:54 +0100

    Completed Book API Setup

---
+++ BookStoreApp.API/Models/Book/BookReadOnlyDto.cs
@@ -0,0 +1,15 @@
+﻿namespace BookStoreApp.API.Models.Book
+{
+    public class BookReadOnlyDto : BaseDto
+    {
+        public string Title { get; set; }
+
+        public string Iamge { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public string AuthorName { get; set; }
+    }
+}
